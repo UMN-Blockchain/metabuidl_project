2e5f97d0ffaaf3b4d3f02bca2a1eb408a4e8c2eb
Kuan <kuanjh123@gmail.com>
2021-09-14 18:17:27 -0500

    Update Passify.sol

--- contracts/Passify.sol
+++ contracts/Passify.sol
@@ -7,12 +7,27 @@ contract Passify {
     struct Record {
         uint8 min; 
         address[] custodians;
-    } // round up to 8 bytes 
+        uint256[] prices;
+    } 
 
     mapping(uint256 => Record) _records; 
+    uint256 _recordsCount;
+    address[] _custodians;
+    uint256 _stakeAmount;
+    
+    // todo: define an event that gets called when user pays 
 
-    // function pay(address[] memory custodians) {
+    function pay(uint256 recordId) external {
+        // make msg.sender transfer agreed payment to each custodian 
+        // delete record after event is emitted 
+    }
 
-    // }
+    function stake() external {
+        // transfer PassifyERC20 to this contract 
+    }
+
+    function addRecord(address[] memory custodians, uint256[] memory prices) external {
+        // create new Record and add to _records 
+    }
 
 }
