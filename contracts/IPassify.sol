39782c52a5a838811778d63199d4e297d18306db
Kuan <kuanjh123@gmail.com>
2021-09-16 23:21:04 -0500

    added interface

---
+++ contracts/IPassify.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.3;
+
+/**
+ @dev Interface of Passify core smart contract
+ */
+interface IPassify {
+
+    /**
+     * @dev Emitted when `user` makes payment to unlock password stored in `recordId`.
+     */
+    event Pay(address indexed user, uint256 indexed recordId);
+
+    /**
+     * @dev Emitted when `user` disputes that `custodian` has returned correct data for `recordId`.
+     */
+    event Dispute(address indexed user, address indexed custodian, uint256 indexed recordId);
+
+
+    /**
+     * @dev User calls this function when they register a new record. No fees paid.
+     */
+    function addRecord(address[] memory custodians) external;
+
+    /**
+     * @dev User calls this function when they wish to unlock their password 
+     * (after verifying their identity).
+     *
+     * Emits a {Pay} event 
+     */
+    function pay(uint256 recordId) external;
+
+    /**
+     * CURRENTLY UNIMPLEMENTED
+     * @dev User calls this function when they contend that custodians have not sent 
+     * the correct information
+     * 
+     * Emits a {Dispute} event 
+     */
+    function dispute(uint256 recordId, uint256 custodianId) external;
+
+    /**
+     * @dev Custodian calls this function to register their service on our platform
+     * Requirement:
+     * msg.value >= fixed stake amount
+     */
+    function stake() payable external;
+}
--- contracts/Passify.sol
+++ contracts/Passify.sol
@@ -1,46 +1,8 @@
 pragma solidity ^0.8.3;
 
+import "./IPassify.sol";
 
-contract Passify {
-
-    struct Record {
-        uint8 min; 
-        address[] custodians;
-    } 
-
-    mapping(uint256 => Record) _records; 
-    uint256 _recordsCount;
-
-    mapping(uint256 => address) _custodians;
-    mapping(uint256 => string) _custodianURIs; // the link to custodian information (stored on IPFS)
-    uint256 _custodiansCount;
-
-    uint256 _stakeAmount; // How much custodians need to stake
-    
-    uint256 _feeAmount; // How much to pay per custodian for unlocking password
-    
-    // called by user to register new record
-    function addRecord(address[] memory custodians, uint256[] memory prices) external {
-        // create new Record and add to _records 
-        // emit an event 
-    }
-
-    // called by user to unlock password
-    function pay(uint256 recordId) external {
-        // make msg.sender transfer agreed payment to each custodian 
-        // emit an event
-        // delete record  
-    }
-
-    // called by user to dispute what the custodians returned
-    function dispute(uint256 recordId, uint256 custodianId) external {
-        // emit an event
-    }
-
-    // called by custodian to register their service
-    function stake() payable external {
-        // transfer ETH to this contract 
-    }
-
-
+// Needs to implement all functions of IPassify 
+contract Passify is IPassify {
+    constructor() IPassify() {}
 }
